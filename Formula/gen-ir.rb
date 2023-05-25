1ee644feb3c35ab8ca2f53c3e420f5b960a62e28
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-05-25 15:19:18 +0200

    gen-ir 0.4.0 formula
    Closes #24.
    
    Signed-off-by: NinjaLikesCheez <1473041+NinjaLikesCheez@users.noreply.github.com>

--- Formula/gen-ir.rb
+++ Formula/gen-ir.rb
@@ -2,8 +2,8 @@ class GenIr < Formula
   desc "Generate LLVM IR from an Xcode Build Log"
   homepage "https://github.com/veracode/gen-ir"
   url "https://github.com/veracode/gen-ir.git",
-    tag:      "0.3.11",
-    revision: "a6f915f82056bf69ec2a3a165e3f9b668c032119"
+    tag:      "0.4.0",
+    revision: "dd082f77961021b8291634aa4703f453af59f1c6"
   version_scheme 1
   head "https://github.com/veracode/gen-ir.git", branch: "main"
 
