9a89b2de87297685fcf43f3a5727b3243a8cede9
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-10-12 14:02:46 +0200

    gen-ir 0.4.3 formula

--- Formula/gen-ir.rb
+++ Formula/gen-ir.rb
@@ -2,8 +2,8 @@ class GenIr < Formula
   desc "Generate LLVM IR from an Xcode Build Log"
   homepage "https://github.com/veracode/gen-ir"
   url "https://github.com/veracode/gen-ir.git",
-    tag:      "0.4.2",
-    revision: "8f44067bfb1194f2fdd5ea7d5f5eee05ab5c63e9"
+    tag:      "0.4.3",
+    revision: "8fde502e75c9079c31e58d028cff8cb375480fef"
   version_scheme 1
   head "https://github.com/veracode/gen-ir.git", branch: "main"
 
