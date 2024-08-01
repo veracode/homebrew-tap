528428c865b8dac342606da1632100097943c115
Shahid <163872978+shahid-veracode@users.noreply.github.com>
2024-08-01 12:04:48 +0530

    Update veracode-cli.rb

--- Formula/veracode-cli.rb
+++ Formula/veracode-cli.rb
@@ -1,43 +1,24 @@
 class VeracodeCli < Formula
-  desc "You use the Veracode CLI to perform various actions for testing the security of your applications."
+  desc "Veracode CLI for security testing of applications"
   homepage "https://www.veracode.com"
-  version "2.28.0"
+  version "2.26.0"
   license "MIT"
-
-  on_macos do
+  if OS.mac?
     if Hardware::CPU.arm?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_macosx_arm64.tar.gz"
-      sha256 "cad9800ef4b592ea150b9b247f244340cd8b6433be68f5b54fc9c6d072f1454f"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_macosx_arm64.tar.gz"
+      sha256 "d3899199dd2f1376621bea4f2f29cddb6a2448a0b374cebdcb7f11d6f2eea4d9"
     elsif Hardware::CPU.intel?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_macosx_x86.tar.gz"
-      sha256 "0557f9353e18f80f32e7e58d202e75efb7d6e0ec83325f8afa49321657216211"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_macosx_x86.tar.gz"
+      sha256 "ba3b01010fa509725f65bd0c4226f325cb9e345c872cdc1c4eae882c84aabedd"
     end
-
-    def install
-      bin.install "veracode"
-    end
+  elsif OS.linux?
+    url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_linux_x86.tar.gz"
+    sha256 "7eb96802d03e20d01760fac823ffa1422c2d5760bbe6063f7503564920c4a1fe"
   end
-
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_linux_x86.tar.gz"
-      sha256 "dd067965fc8b59f58f6e6aa98fbeda464274f5b9abeefb0f758c0090fd774dd3"
-
-      def install
-        bin.install "veracode"
-      end
-    fi
-    if Hardware::CPU.intel?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_linux_x86.tar.gz"
-      sha256 "dd067965fc8b59f58f6e6aa98fbeda464274f5b9abeefb0f758c0090fd774dd3"
-
-      def install
-        bin.install "veracode"
-      end
-    fi
+  def install
+    bin.install "veracode"
   end
-
   test do
-    system "#{bin}/veracode --version"
+    system "#{bin}/veracode", "version"
   end
 end
